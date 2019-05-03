ce5772baea167a1cfb8b7a6cf0360f9392e09417
Satya Prakash <33595708+satya-p91@users.noreply.github.com>
2019-05-03 16:54:02 +0530

    Update config.php
    removed firebase server key

--- server app/include/config.php
+++ server app/include/config.php
@@ -7,7 +7,7 @@ define('DB_PASSWORD', 'password');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'chat');
  
-define("FCM_API_KEY", "AAAArx_zol0:APA91bG4JehqmFLjt3eM0DHBtltZ4SAiEgKo3PBG5tS5wUUCYJoFrKkanUsyPvB1cXI5mwTFUeHYgUiIM-TVkL7F07XNYobrzib_eIg0jqlAZhopnj2mLl8ItevkPBOB2G9jP8iETcx9");
+define("FCM_API_KEY", "AAAArx_zol0:APA91bG4JehqmFLjt3eM0DHBtltZ4SAiEgKo3PBG5tS5wUUCYJoFrKkanUsyPvB1cXI5mwTFUeHYgUiIM-TVkL7F07XNYobrzib_eIg0jqlAZhopnj2mLl8ItevkPBOB2G");
  
 // push notification flags
 define('PUSH_FLAG_CHATROOM', 1);
