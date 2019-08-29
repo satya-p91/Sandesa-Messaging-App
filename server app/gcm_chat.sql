77f9f8bd0fe48d790046b83a6be0189fc32753bc
Satya Prakash <33595708+satya-p91@users.noreply.github.com>
2019-08-29 20:00:16 +0530

    Added sample mysql database file for server application

--- server app/gcm_chat.sql
+++ server app/gcm_chat.sql
@@ -1,17 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.4.10
--- http://www.phpmyadmin.net
---
--- Host: localhost:3306
--- Generation Time: Feb 09, 2016 at 07:38 AM
--- Server version: 5.5.42
--- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `gcm_chat`
 --
 
 -- --------------------------------------------------------
